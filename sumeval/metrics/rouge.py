631a3e3e9f87e0e59aef478d2a0396a995ae015b
Yu-Yang Huang <yuyang.gnauh@gmail.com>
2020-12-09 01:02:49 +0800

    Implement summary-level LCS ROUGE

--- sumeval/metrics/rouge.py
+++ sumeval/metrics/rouge.py
@@ -1,13 +1,13 @@
+from typing import List, Union
 from collections import Counter
+
 from sumeval.metrics.lang.base_lang import BaseLang
 from sumeval.metrics.lang import get_lang
 
 
-class RougeCalculator():
+class RougeCalculator:
 
-    def __init__(self,
-                 stopwords=True, stemming=False,
-                 word_limit=-1, length_limit=-1, lang="en"):
+    def __init__(self, stopwords=True, stemming=False, word_limit=-1, length_limit=-1, lang="en"):
         self.stemming = stemming
         self.stopwords = stopwords
         self.word_limit = word_limit
@@ -91,7 +91,7 @@ class RougeCalculator():
 
     def ngram_iter(self, words, n):
         for i in range(self.len_ngram(words, n)):
-            n_gram = words[i:i+n]
+            n_gram = words[i:i + n]
             yield tuple(n_gram)
 
     def count_ngrams(self, words, n):
@@ -154,72 +154,128 @@ class RougeCalculator():
         denom = (1.0 - alpha) * precision + alpha * recall
         return safe_div(precision * recall, denom)
 
-    def lcs(self, a, b, w=1.):
-        longer = a
-        base = b
-        if len(longer) < len(base):
-            longer, base = base, longer
+    def _lcs_table(self, reference, candidate, w=1.):
+        """Create 2-d LCS score table."""
+        rows = len(reference)
+        cols = len(candidate)
 
-        if len(base) == 0:
+        lcs_table = [[0] * (cols + 1) for _ in range(rows + 1)]
+        consecutive_table = [[0] * (cols + 1) for _ in range(rows + 1)]  # for ROUGE-W
+
+        for i in range(1, rows + 1):
+            for j in range(1, cols + 1):
+                if reference[i - 1] == candidate[j - 1]:
+                    num_consecutive = consecutive_table[i - 1][j - 1]
+                    consecutive_score = (num_consecutive + 1) ** w - num_consecutive ** w
+                    lcs_table[i][j] = lcs_table[i - 1][j - 1] + consecutive_score
+                    consecutive_table[i][j] = num_consecutive + 1
+                else:
+                    lcs_table[i][j] = max(lcs_table[i - 1][j], lcs_table[i][j - 1])
+                    consecutive_table[i][j] = 0
+
+        return lcs_table
+
+    def union_lcs_ind(self, reference: List[str], candidates: List[List[str]],
+                      w: float = 1.) -> List[str]:
+        return sorted({i for c in candidates for i in self.lcs_ind(reference, c, w=w)})
+
+    def lcs_ind(self, reference: List[str], candidate: List[str], w: float = 1.) -> List[int]:
+        def backtrack(table):
+            """Read out LCS indices."""
+            i = len(reference)
+            j = len(candidate)
+            lcs = []
+            while i > 0 and j > 0:
+                if reference[i - 1] == candidate[j - 1]:
+                    lcs.insert(0, i - 1)
+                    i -= 1
+                    j -= 1
+                elif table[i][j - 1] > table[i - 1][j]:
+                    j -= 1
+                else:
+                    i -= 1
+            return lcs
+
+        lcs_table = self._lcs_table(reference, candidate, w=w)
+        return backtrack(lcs_table)
+
+    def lcs(self, reference: List[str], candidate: List[str], w: float = 1.) -> int:
+        if len(reference) == 0 or len(candidate) == 0:
             return 0
 
-        row = [0] * len(base)
-        matches = [0] * len(base)  # consecutive matches for ROUGE-W
-        for c_a in longer:
-            left = 0
-            upper_left = 0
-            matches_upper_left = 0
-            for i, c_b in enumerate(base):
-                up = row[i]
-                matches_up = matches[i]
-                if c_a == c_b:
-                    value = upper_left + (matches_upper_left + 1) ** w - matches_upper_left ** w
-                    matches[i] = matches_upper_left + 1
-                else:
-                    value = max(left, up)
-                    matches[i] = 0
-                row[i] = value
-                left = value
-                upper_left = up
-                matches_upper_left = matches_up
+        lcs_table = self._lcs_table(reference, candidate, w=w)
+        return lcs_table[-1][-1]
 
-        return left
+    def rouge_l(self, *args, **kwargs):
+        return self.rouge_w(w=1., *args, **kwargs)
 
-    def rouge_l(self, summary, references, alpha=0.5):
-        return self.rouge_w(summary, references, alpha, w=1.)
+    def rouge_w(self, summary: str, references: Union[str, List[str]],
+                alpha: float = 0.5, w: float = 1.2, summary_level: bool = False):
+        """Calculate ROUGE-W score.
 
-    def rouge_w(self, summary, references, alpha=0.5, w=1.2):
-        """
-        Calculate ROUGE-W score.
+        To compute summary-level ROUGE-L, join the summary sentences with the
+        newline character and set ``summary_level=True``.
 
         Parameters
         ----------
-        summary: str
+        summary : str
             summary text
-        references: str or str[]
+        references : str or List[str]
             reference or references to evaluate summary
-        alpha: float (0~1)
+        alpha : float
             alpha -> 0: recall is more important
             alpha -> 1: precision is more important
-            F = 1/(alpha * (1/P) + (1 - alpha) * (1/R))
-        w: float
-            Set to 1 for ROUGE-L.
+            F = 1 / (alpha * (1 / P) + (1 - alpha) * (1 / R))
+        w : float
+            The importance factor for consecutive LCS matches. Set to 1 for ROUGE-L.
+        summary_level : bool
+            Whether to use summary-level evaluation instead of sentence-level.
+            Defaults to False.
 
         Returns
         -------
-        f1: float
-            f1 score
+        f1 : float
+            The F1 score.
         """
+        if isinstance(references, str):
+            references = [references]
+        reference_tokens = [self.tokenize(x, is_reference=True) for x in references]
+
+        if summary_level:
+            candidate_tokens = [self.tokenize(x) for x in summary.split('\n')]
+        else:
+            candidate_tokens = [self.tokenize(summary)]
+
+        # Prevent double-counting
+        reference_token_counter = Counter(x for tokens in reference_tokens for x in tokens)
+        candidate_token_counter = Counter(x for tokens in candidate_tokens for x in tokens)
+
+        total_reference_tokens = sum(reference_token_counter.values())
+        total_candidate_tokens = sum(candidate_token_counter.values())
+
         matches = 0
-        count_for_recall = 0
-        _summary = self.tokenize(summary)
-        _refs = [references] if isinstance(references, str) else references
-        for r in _refs:
-            _r = self.tokenize(r, True)
-            matches += self.lcs(_r, _summary, w=w)
-            count_for_recall += len(_r)
-        count_for_prec = len(_refs) * len(_summary)
-        f1 = self._calc_f1(matches, count_for_recall, count_for_prec, alpha, w=w)
+        for ref in reference_tokens:
+            if summary_level:
+                num_consecutive = 0
+                last_idx = None
+                for idx in self.union_lcs_ind(ref, candidate_tokens, w=w):
+                    token = ref[idx]
+                    if reference_token_counter[token] > 0 and candidate_token_counter[token] > 0:
+                        consecutive_score = (num_consecutive + 1) ** w - num_consecutive ** w
+                        matches += consecutive_score
+
+                        if last_idx is None or last_idx + 1 == idx:
+                            num_consecutive += 1
+                        else:
+                            num_consecutive = 0
+                        last_idx = idx
+
+                        reference_token_counter[token] -= 1
+                        candidate_token_counter[token] -= 1
+            else:
+                matches += self.lcs(ref, candidate_tokens[0], w=w)
+
+        f1 = self._calc_f1(matches, total_reference_tokens, total_candidate_tokens, alpha, w=w)
         return f1
 
     def count_be(self, text, compare_type, is_reference=False):
